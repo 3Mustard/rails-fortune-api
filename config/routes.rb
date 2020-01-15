3b2e1ac2cc6abfc43ba97e5e15ac50da74ff4167
3Mustard <m23.cullen@gmail.com>
2020-01-15 14:21:15 -0500

    test passed: js file fetches from rails api

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :notes
   resources :fortunes
   resources :cards
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #testing purposes
+  get '/test', to: 'application#test'
+
+
 end
