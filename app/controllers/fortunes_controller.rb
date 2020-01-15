a369f9f4339b2a0e4c48dd5639ad205a3faa08be
3Mustard <m23.cullen@gmail.com>
2020-01-15 15:14:08 -0500

    add some crud to controllers

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,2 +1,25 @@
 class FortunesController < ApplicationController
+
+    #see all fortune data
+    def index 
+        fortunes = Fortune.all 
+
+        render json: fortunes 
+    end 
+    #create a new fortune
+    def create 
+        fortune = Fortune.new
+        #fortune.drawThree()
+        #fortune.save
+        #render json: fortune
+    end 
+    #destroy a saved fortune 
+    def destroy 
+        fortune = Fortune.find_by(id: params[:id])
+        fortune.destroy
+        render json: fortune 
+    end 
 end
+pokemon = Pokemon.find_by(id: params[:id])
+        pokemon.destroy
+        render json: pokemon
